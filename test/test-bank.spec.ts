b114244e073abef99dfc7d0ba85c4e35be647a78
Jeff Tian <jie.tian@unicareer.com>
2019-11-13 14:24:54 +0800

    refactor: refactor screenRecording

--- test/test-bank.spec.ts
+++ test/test-bank.spec.ts
@@ -1,7 +1,25 @@
-import assert = require('assert');
+// import assert = require('assert');
+import puppeteer from 'puppeteer'
+import screenRecording from "./recording";
+
+let page;
+let browser;
 
 describe('test-bank', () => {
-    it('hello', () => {
-        assert('hell')
+    beforeAll(async () => {
+        browser = await puppeteer.launch({
+            headless: 'true' === process.env.ci,
+        });
+        page = await browser.newPage()
+
+        afterAll(browser.close)
+    });
+
+    it('hello', async () => {
+        async function testing() {
+            await page.goto('https://www.unicareer.com/test-bank/', {waitUntil: 'networkidle2'});
+        }
+
+        await Promise.all([testing, () => screenRecording(page, null, null)])
     })
 })
