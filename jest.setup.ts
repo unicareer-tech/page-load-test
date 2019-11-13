26ad39c7063250562a08dc694d11eecee67beed1
Jeff Tian <jie.tian@unicareer.com>
2019-11-13 16:00:05 +0800

    type username and password

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,2 @@
 import "expect-puppeteer";
+jest.setTimeout(120000); // in milliseconds
--- test/test-bank.spec.ts
+++ test/test-bank.spec.ts
@@ -1,35 +1,25 @@
-// import assert = require('assert');
-import puppeteer from 'puppeteer'
-import screenRecording from "./recording";
-
-let page;
-let browser;
-let width = 1080, height = 800;
+import screenRecording, {sleep} from "./recording";
 
 describe('test-bank', () => {
     beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: 'true' === process.env.ci,
-            args: [`--window-size=${width},${height}`]
-        });
-
-        page = await browser.newPage()
-        await page.setViewport({width, height})
-        console.log('set view port')
-
-        afterAll(browser.close)
-    });
+        await page.goto('https://www.unicareer.com/test-bank/', {waitUntil: 'networkidle0'})
+    })
 
     it('hello', async () => {
         async function testing() {
-            await page.goto('https://www.unicareer.com/test-bank/', {waitUntil: 'networkidle2'});
-            const client = await page.target().createCDPSession()
+            await page.tap('input[type=number]')
+            await page.keyboard.type('17717373367')
+            await page.tap('input[type=password')
+            await page.keyboard.type('UniCareer@2018')
+            await page.tap('button[type=submit]')
+            await page.waitFor('div.list')
+            await sleep(5);
         }
 
         await Promise.all([
             testing(),
-            screenRecording(page, width, height)
-        ])
+            'true' === process.env.ci ? () => screenRecording(page, page.viewport().width, page.viewport().height) : () => Promise.resolve('hello')
+        ]);
 
         console.log('test done.')
     })
